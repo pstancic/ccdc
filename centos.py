6ec7986f797e8975d32779d71c2ca65b01c02db4
Peter Stancic <peter.stancic.502@my.csun.edu>
2018-10-29 21:59:07 -0700

    quick fixes

--- centos.py
+++ centos.py
@@ -3,25 +3,32 @@ import glob
 import sys
 import re
 
-os.system("systemctl disable firewalld")
-os.system("systemctl stop firewalld")
-os.system("systemctl enable iptables")
-os.system("systemctl start iptables")
-
 if (len(sys.argv) < 2 or os.getuid() != 0):
     print """
     This script will set up interfaces on CentOS Router and MUST be run as root
     Usage: python centos.py teamNumber
     """
     exit()
+
+os.system("yum install libssh tcpdump wget curl htop iptables iptables-service net-tools bind-utils nano lsof -y")
+
+os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+os.system("sysctl -w net.ipv4.ip_forward=1")
+os.system("systemctl disable firewalld")
+os.system("systemctl stop firewalld")
+os.system("systemctl enable iptables")
+os.system("systemctl start iptables")
+os.system("iptables -F")
+
 base_dir = '/etc/sysconfig/network-scripts/'
 interfaces = glob.glob(base_dir + 'ifcfg-e*')
 
 teamNum = str(sys.argv[1])
 
 ip = list()
-ip[0] = "10.10." + teamNum + ".1"
-ip[1] = "192.168.22.1"
+ip[0] = "10.47.1" + teamNum + ".2"
+ip[1] = "10.57.1" + teamNum + ".1"
+ip[2] = "192.168.220.1"
 
 if (len(interfaces) == 2):
     for i in range(len(interfaces)):
@@ -34,19 +41,33 @@ if (len(interfaces) == 2):
         name = re.match(r'NAME=(\S+)', string)
         device = re.match(r'DEVICE=(\S+)', string)
         UUID = re.match(r'UUID=(\S+)', string)
-        config_string = """
-        TYPE=Ethernet
-        PROXY_METHOD=none
-        BOOTPROTO=static
-        DEFROUTE=yes
-        IPV4_FAILURE_FATAL=no
-        IPV6INIT=no
-        ONBOOT=yes
-        """
-        config_string += "\nNAME=" + str(name[1]) + "\nDEVICE=" + str(device[1]) + "\nUUID=" + str(UUID[1])
-        config_string += "\nIPADDR=" + ip[i] + "\nNETMASK=255.255.255.0"
         if (i == 0):
-            config_string += "\nGATEWAY=10.10." + teamNum + ".2"
+            config_string = """
+            TYPE=Ethernet
+            PROXY_METHOD=none
+            BOOTPROTO=static
+            DEFROUTE=yes
+            IPV4_FAILURE_FATAL=no
+            IPV6INIT=no
+            ONBOOT=yes
+            """
+            config_string += "\nNAME=" + str(name[1]) + "\nDEVICE=" + str(device[1]) + "\nUUID=" + str(UUID[1])
+            config_string += "\nIPADDR0=" + ip[i] + "\nPREFIX0=30"
+            config_string += "\nGATEWAY=10.47.1" + teamNum + ".1"
+            config_string += "\nIPADDR1=" + ip[i+1] + "\nPREFIX1=24"
+        else:
+            config_string = """
+            TYPE=Ethernet
+            PROXY_METHOD=none
+            BOOTPROTO=static
+            DEFROUTE=yes
+            IPV4_FAILURE_FATAL=no
+            IPV6INIT=no
+            ONBOOT=yes
+            NAME=
+            """
+            config_string += str(name[1]) + "\nDEVICE=" + str(device[1]) + "\nUUID=" + str(UUID[1])
+            config_string += "\nIPADDR=" + ip[i+1] + "\nNETMASK=255.255.255.0"
         f = open(interfaces[i], 'w')
         f.write(config_string)
         f.close()
